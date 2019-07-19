1aa71eee751e87e011cb282d2079fedbe17f2021
MikBar2501 <45071328+MikBar2501@users.noreply.github.com>
2019-07-19 23:43:43 +0200

    Initial commit

---
+++ BeeColonyOptimization/BeeColonyOptimization/Program.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace BeeColonyOptimization
+{
+
+    public struct Bee
+    {
+        //status: true -> zadeklarowana, false -> niezadeklarowana
+        public bool status;
+        public List<UInt32> path;
+        public double pathValue;
+        public double normalizeValue;
+        public double recruitValue;
+
+
+        public void SetPath(List<UInt32> newPath)
+        {
+            path = newPath;
+        }
+
+        public void ResetNormalizeAndRecruitValue()
+        {
+            normalizeValue = 0;
+            recruitValue = 0;
+        }
+    }
+
+    class Program
+    {
+
+        public static Random rand = new Random();
+        public static Values values;
+
+        static void Main(string[] args)
+        {
+            string pathFile = @"TSP\kroA100.tsp";
+            Point[] points = TSPFileReader.ReadTspFile(pathFile);
+            Graph graph = new Graph(points);
+
+            Console.WriteLine("Set count of bees: ");
+            UInt32 bees = UInt32.Parse(Console.ReadLine());
+   
+            values = new Values(bees, pathFile, "BCO");
+            values.StartTime();
+            Solve(graph, bees, graph.dimension, 3);
+            
+            values.StopTime();
+            Console.WriteLine("End");
+
+            Console.ReadKey();
+
+        }
+
+        public static double FindBestPath(Bee[] hive, Graph graph)
+        {
+            double best = graph.CalculateRouteLength(hive[0].path);
+            for (int i = 1; i < hive.Length; i++)
+            {
+                double nextBeePath = graph.CalculateRouteLength(hive[i].path);
+                if (best > nextBeePath)
+                {
+                    best = nextBeePath;
+                }
+            }
+            return best;
+
+        }
+
+        public static void Solve(Graph graph, UInt32 bees, UInt32 iterations, UInt32 steps)
+        {
+            List<UInt32> bestPath = new List<UInt32>();
+            double newPath = 0;
+
+            for (int k = 0; k < graph.dimension; k++)
+            {
+                bestPath.Add((UInt32)k);
+            }
+            double bestPathLength = graph.CalculateRouteLength(bestPath);
+            Console.WriteLine("Best path is: " + bestPathLength);
+            //Values.toFile.Add("-First best-" + 0 + "-" + bestPathLength);
+            Values.AddNewValues(0, bestPathLength);
+            Bee[] hive = new Bee[bees];
+            for(int j = 1; j < iterations; j++)
+            {
+                hive = ResetHive(hive, graph);
+                for (int i = 0; i < graph.dimension; i++)
+                {
+                    for (int s = 0; s < steps; s++)
+                    {
+                        for (int b = 0; b < hive.Length; b++)
+                        {
+                            if (hive[b].path.Count == graph.dimension) continue;
+                            UInt32 nextPosition = ChooseNextNode(graph, hive[b]);
+                            hive[b].path.Add(nextPosition);
+                        }
+                    }
+
+                    for (int b = 0; b < hive.Length; b++)
+                    {
+                        hive[b].pathValue = graph.CalculateRouteLength(hive[b].path);
+                    }
+
+
+
+                    hive = Loyalty(hive);
+                    foreach(Bee bee in hive)
+                    {
+                        bee.ResetNormalizeAndRecruitValue();
+                    }
+
+                    hive = Recruit(hive);
+                    foreach (Bee bee in hive)
+                    {
+                        bee.ResetNormalizeAndRecruitValue();
+                    }
+
+                    newPath = FindBestPath(hive, graph);
+
+                    if (newPath < bestPathLength)
+                    {
+                        bestPathLength = newPath;
+                        Console.WriteLine("New best found in iteration: " + j.ToString() + ", new best is: " + bestPathLength);
+                        //Values.toFile.Add("-New best-" + j.ToString() + "-" + bestPathLength);
+                        Values.AddNewValues(j, bestPathLength);
+                    }
+                }
+            }
+        }
+
+        public static Bee [] ResetHive(Bee [] hive, Graph graph)
+        {
+            for (int i = 0; i < hive.Length; i++)
+            {
+                hive[i].status = true;
+                hive[i].path = new List<UInt32>();
+                hive[i].pathValue = 0;
+                hive[i].normalizeValue = 0;
+                hive[i].recruitValue = 0;
+            }
+
+            for (int b = 0; b < hive.Length; b++)
+            {
+                UInt32 startPosition = (UInt32)rand.Next(0, (int)graph.dimension);
+                hive[b].path.Add(startPosition);
+            }
+
+            return hive;
+        }
+
+
+        public static UInt32 ChooseNextNode(Graph g, Bee bee)
+        {
+
+            List<UInt32> nodeList = new List<UInt32>();
+            for (UInt32 i = 0; i < g.dimension; i++)
+            {
+                nodeList.Add(i);
+            }
+
+            foreach (UInt32 node in bee.path)
+            {
+                nodeList.Remove(node);
+            }
+
+            /*foreach (UInt32 node in nodeList)
+            {
+                Console.Write(node);
+                Console.Write(", ");
+            }
+            Console.WriteLine("___________________");*/
+
+            int index = rand.Next(0, nodeList.Count);
+
+            UInt32 nextNode = nodeList[index];
+
+            return nextNode;
+        }
+
+        public static Bee [] Loyalty(Bee [] bees)
+        {
+
+            // wartość min = A, wartość max = B, znormalizowana min = a -> 0, znormalizowana max = b -> 1, x = warość
+            //a + (x-A)(b-a)/(B-A)
+            IEnumerable<Bee> sortedBees = from bee in bees orderby bee.pathValue select bee;
+            bees = sortedBees.ToArray();
+            double maxValue = bees[bees.Length - 1].pathValue;
+            double minValue = bees[0].pathValue;
+
+            bees[0].normalizeValue = 0;
+            bees[bees.Length-1].normalizeValue = 1;
+
+            for(int i = 1; i < bees.Length-1; i++)
+            {
+                bees[i].normalizeValue = NormalizeValue(minValue, maxValue, 0, 1, bees[i].pathValue);
+            }
+
+
+            for(int i = 0; i < bees.Length; i++)
+            {
+                double loyalty = Math.Pow(Math.E, -(1 - bees[i].normalizeValue));
+                if (loyalty < 0.7)
+                {
+                    bees[i].status = false;
+                }
+                
+            }
+
+            return bees;
+
+        }
+
+        public static double NormalizeValue(double minValue, double maxValue, double normalizeMin, double normalizeMax, double value)
+        {
+            return normalizeMin + (((value + minValue) * (normalizeMax - normalizeMin)) / (maxValue - minValue));
+        }
+
+        public static Bee [] Recruit(Bee [] bees)
+        {
+            List<Bee> recruiters = new List<Bee>();
+            List<Bee> unemployed = new List<Bee>();
+            foreach (Bee bee in bees)
+            {
+                if(bee.status == true)
+                {
+                    recruiters.Add(bee);
+                } else
+                {
+                    unemployed.Add(bee);
+                }
+            }
+
+            IEnumerable<Bee> sortedBees = from bee in recruiters orderby bee.pathValue select bee;
+            Bee [] recruitersArray = sortedBees.ToArray();
+            Bee[] uneployedArray = unemployed.ToArray();
+            double maxValue = recruitersArray[recruitersArray.Length - 1].pathValue;
+            double minValue = recruitersArray[0].pathValue;
+            recruitersArray[0].normalizeValue = 0;
+            recruitersArray[recruitersArray.Length - 1].normalizeValue = 1;
+            double sumValues = 0;
+            for (int i = 0; i < recruitersArray.Length; i++)
+            {
+                recruitersArray[i].normalizeValue = NormalizeValue(minValue, maxValue, 0, 1, bees[i].pathValue);
+                sumValues += recruitersArray[i].normalizeValue;
+            }
+
+            for (int i = 0; i < recruitersArray.Length; i++)
+            {
+                recruitersArray[i].recruitValue = recruitersArray[i].normalizeValue / sumValues;
+            }
+
+            IEnumerable<Bee> sortedRecruiterBees = from bee in recruitersArray orderby bee.pathValue select bee;
+            recruitersArray = sortedRecruiterBees.ToArray();
+
+
+            for(int b = 0; b < uneployedArray.Length; b++)
+            {
+                uneployedArray[b].SetPath(ChooseRecruiter(recruitersArray));
+                uneployedArray[b].status = true;
+
+
+            }
+
+            Bee[] hive = recruitersArray.Concat(uneployedArray).ToArray();
+            return hive;
+
+        }
+
+        public static List<UInt32> ChooseRecruiter(Bee[] bees)
+        {
+            double choose = rand.NextDouble();
+            bool find = false;
+            Bee recruiter = new Bee();
+            //int i = 1;
+
+            
+            for(int i = 0; i < bees.Length - 1; i++)
+            {
+                if(bees[i].recruitValue < choose && bees[i + 1].recruitValue > choose && !find)
+                {
+                    double valueBetween = (bees[i].recruitValue + bees[i+1].recruitValue) / 2;
+                    if (choose >= valueBetween)
+                    {
+                        recruiter = bees[i+1];
+                        find = true;
+                    }
+                    else
+                    {
+                        recruiter = bees[i];
+                        find = true;
+                    }
+
+                } else
+                {
+                    recruiter = bees[bees.Length - 1];
+                }
+            }
+
+
+            return recruiter.path;
+
+        }
+        
+    }
+}
+
---
+++ BeeColonyOptimization/BeeColonyOptimization/Values.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace BeeColonyOptimization
+{
+    class Values
+    {
+        double bestFunctionValue;
+        UInt32 bestFunctionIteration;
+        Stopwatch stopWatch;
+        TimeSpan timeSpan;
+        string elapsedTime;
+        public static List<string> toFile = new List<string>();
+        string algorithm;
+        string problemName;
+        string path;
+        public static UInt32 beesCount;
+
+        public Values ()
+        {
+            stopWatch = new Stopwatch();
+        }
+
+        //Dla BCO
+        public Values(UInt32 countOfBees, string pathName, string algorithm)
+        {
+            stopWatch = new Stopwatch();
+            this.algorithm = algorithm;
+            path = pathName;
+            beesCount = countOfBees;
+            toFile.Add("Bee Colony Optimization for " + path);
+            toFile.Add("Count of bees: |" + countOfBees);
+        }
+
+        public void StartTime()
+        {
+            stopWatch.Start();
+        }
+
+        public void StopTime()
+        {
+            stopWatch.Stop();
+            timeSpan = stopWatch.Elapsed;
+            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds / 10);
+            toFile.Add("Time: " + elapsedTime);
+            SaveToFile(toFile, algorithm, SplitName(path));
+        }
+
+        public static void SaveToFile(List<String> tofile, string algorithm, string startFile)
+        {
+            DateTime dt = DateTime.Now;
+            string fileName = String.Format("{0:dd-MM-yyyy-hh-mm-ss}", dt) + "-" + algorithm + "-" + startFile + "-" + beesCount.ToString();
+            File.Create(@"Files\" + fileName + ".txt").Dispose();
+            using (StreamWriter sw = new StreamWriter(@"Files\" + fileName + ".txt"))
+            {
+                foreach (string line in tofile)
+                {
+                    sw.WriteLine(line);
+                }
+            }
+        }
+
+        public void AddToFile(string text)
+        {
+            toFile.Add(text);
+        }
+
+        public string SplitName(string pathFile)
+        {
+            string[] path = pathFile.Split('\\');
+            string[] name = path[path.Length - 1].Split('.');
+            return name[0];
+        }
+
+        public static void AddNewValues(int iteration, double value)
+        {
+            toFile.Add("-New best-" + iteration + "-" + value);
+        }
+
+
+
+
+    }
+}
